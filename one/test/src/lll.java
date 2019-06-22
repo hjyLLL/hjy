4515bbb4767332a53707a94909d3d3230b99d044
hjyLLL <L893032405@163.com>
2019-06-22 21:30:05 +0800

    idea

---
+++ one/test/src/lll.java
@@ -0,0 +1,11 @@
+/**
+ * @author ：L
+ * @date ：Created in 2019/6/22
+ * @description ：
+ * @version: 1.0
+ */
+public class lll {
+    public static void main(String[] args) {
+        System.out.println("lllllllll啦啦啦");
+    }
+}
